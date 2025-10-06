82c9076b33ec0931fc59bbb5f4177df15539aa86
GianninaGit <giannisarappa@gmail.com>
2025-10-06 19:48:22 +0200

    Initial commit and setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# InnotIT Automation Test
+
+This repository contains an automated suit of test using Cypress to test the web application The Internet.
+The goal of this project is to desmostrate automation testing skills. 
+
+
---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}", 
+    supportFile: "cypress/support/e2e.js",
+  },
+});
